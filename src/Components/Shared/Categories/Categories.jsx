8385be7c4bbc2fd0b29da92167f7108245fd78cf
ares-oykot <soykotislam131415@gmail.com>
2023-11-06 15:14:27 +0600

    The job categories section is made responsive for tablet devices and mobile devices

--- src/Components/Shared/Categories/Categories.jsx
+++ src/Components/Shared/Categories/Categories.jsx
@@ -3,10 +3,12 @@ import 'react-tabs/style/react-tabs.css';
 import GraphicsDesign from './GraphicsDesign';
 import WebDevelopment from './WebDevelopment';
 import DigitalMarketing from './DigitalMarketing';
+
 const Categories = () => {
     return (
-        <Tabs className="max-w-screen-xl mx-auto">
-            <TabList className="text-center">
+        <Tabs className="max-w-screen-xl mx-2 md:mx-auto">
+            <h1 className='text-center text-3xl md:text-4xl my-2 md:my-3 font-black'>Our Jobs Categories</h1>
+            <TabList className="text-center font-bold text-[9px] md:text-base">
                 <Tab>Web Development</Tab>
                 <Tab>Digital Marketing</Tab>
                 <Tab>Graphics Design</Tab>
--- src/Components/Shared/Categories/WebDevelopment.jsx
+++ src/Components/Shared/Categories/WebDevelopment.jsx
@@ -8,22 +8,22 @@ const WebDevelopment = () => {
             .then(data => setJobs(data));
     }, []);
     return (
-        <div className="grid grid-cols-2 gap-5 mt-2">
+        <div className="grid md:grid-cols-2 gap-5 mt-[6px]">
             {
                 jobs.map(job => <div key={job._id}>
                     <div className="bg-gradient-to-r from-[#b7abff] to-[#94c6ff] flex flex-col justify-between hover:shadow-xl rounded-md h-full duration-200">
                         <div className="">
-                            <img className="w-full rounded-t-md h-80" src={job.img} alt="" />
-                            <div className="mx-10 mt-3">
-                                <h4 className="text-2xl font-semibold">{job.title}</h4>
-                                <div className="text-slate-700 mt-2">
-                                    <h6>Deadline: {job.deadline}</h6>
-                                    <h6>Price Range: {job.price_range}</h6>
-                                    <p>Description: {job.short_description}</p>
+                            <img className="w-full rounded-t-md h-40 md:h-52 lg:h-80" src={job.img} alt="" />
+                            <div className="mx-2 md:mx-5 lg:mx-10 mt-3">
+                                <h4 className="text-xl md:text-xl lg:text-2xl font-semibold">{job.title}</h4>
+                                <div className="text-slate-700 text-xs md:text-sm lg:text-base mt-2">
+                                    <h6><span className="text-black font-semibold">Deadline:</span> {job.deadline}</h6>
+                                    <h6><span className="text-black font-semibold">Price Range:</span> {job.price_range}</h6>
+                                    <p><span className="text-black font-semibold">Description:</span> {job.short_description}</p>
                                 </div>
                             </div>
                         </div>
-                        <button className="mx-10 mt-2 rounded bg-gradient-to-t from-[#73a8e4] to-[#897ade] mb-10 btn">Bid Now</button>
+                        <button className="mx-3 md:mx-5 lg:mx-10 mt-2 md:text-sm lg:text-base rounded bg-gradient-to-t from-[#73a8e4] to-[#897ade] mb-3 md:mb-5 lg:mb-10 btn">Bid Now</button>
                     </div>
                 </div>)
             }
